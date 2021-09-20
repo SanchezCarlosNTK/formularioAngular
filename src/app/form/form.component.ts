6c745ef1d91ac215ab49ac03ad2b4cbaef52c14b
Carlos Andrés Sánchez Vázquez <carlos.sanchez@NTKAD20.local>
2021-09-20 15:57:28 -0500

    Borrar residuosde codigo inservible

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -31,22 +31,18 @@ form;
 		{ name: 'Argentina', states: [ {name: 'C', cities: ['Buenos aires','Mar delplata','Santa fe']} ] },
 	];
 
-   //states: Array<any>; //Angular 8
-	states: Array<any> = []; //Angular 11
+	states: Array<any> = []; 
 
-	//cities: Array<any>; //Angular 8
-	cities: Array<any> = []; //Angular 11
+	cities: Array<any> = []; 
 	
-	//changeCountry(country) { //Angular 8
-	changeCountry(country: any) { //Angular 11
-		//this.states = this.Countries.find(cntry => cntry.name == country).states; //Angular 8
-		this.states = this.Countries.find((cntry: any) => cntry.name == country.target.value).states; //Angular 11
+	changeCountry(country: any) { 
+		this.states = this.Countries.find((cntry: any) => cntry.name == country.target.value).states; 
 	}
 
-	//changeState(state) { //Angular 8
-	changeState(state: any) { //Angular 11
-		//this.cities = this.Countries.find(cntry => cntry.name == this.selectedCountry).states.find(stat => stat.name == state).cities; //Angular 8
-		this.cities = this.Countries.find((cntry: any) => cntry.name == this.selectedCountry).states.find((stat: any) => stat.name == state.target.value).cities; //Angular 11
+	
+	changeState(state: any) { 
+	
+		this.cities = this.Countries.find((cntry: any) => cntry.name == this.selectedCountry).states.find((stat: any) => stat.name == state.target.value).cities; 
 	}
 
 
